5d7c3438b9f8e922c82043b6a9349e872477b28a
qkaguayo <kuka.aguayo@gmail.com>
2021-09-29 10:35:47 -0300

    cambios

--- formulario-equipo/src/app/app.module.ts
+++ formulario-equipo/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
 import { RegistroComponent } from './component/registro/registro.component';
-import { HomeComponent } from './component/home/home.component';
 import { ProductosComponent } from './component/productos/productos.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './compartida/header/header.component';
@@ -17,7 +16,6 @@ import { HttpClientModule} from '@angular/common/http';
     AppComponent,
     LoginComponent,
     RegistroComponent,
-    HomeComponent,
     ProductosComponent,
     HeaderComponent,
     FooterComponent
