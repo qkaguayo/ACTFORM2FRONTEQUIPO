ad73d7d61f0c217d3979364e4652af0f2d9ed9ad
qkaguayo <kuka.aguayo@gmail.com>
2021-09-29 09:23:18 -0300

    creacion componentes

--- formulario-equipo/src/app/app.module.ts
+++ formulario-equipo/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegistroComponent } from './component/registro/registro.component';
+import { HomeComponent } from './component/home/home.component';
+import { ProductosComponent } from './component/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    HomeComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
