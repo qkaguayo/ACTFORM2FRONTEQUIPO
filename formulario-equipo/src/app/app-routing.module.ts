51b3501e4148aba784662f07eb23baca06da423f
Tostin <tostin737@gmail.com>
2021-09-29 11:59:43 -0300

    subida

--- formulario-equipo/src/app/app-routing.module.ts
+++ formulario-equipo/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { ProductosComponent } from './component/productos/productos.component';
+import { RegistroComponent } from './component/registro/registro.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path:'producto', component: ProductosComponent}
+  {path:'producto', component: ProductosComponent},
+  {path: 'registro', component: RegistroComponent}
 ];
 
 @NgModule({
