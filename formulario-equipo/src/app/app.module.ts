047ace856ae58692b49f07b919205b8e9f1902b6
qkaguayo <kuka.aguayo@gmail.com>
2021-09-29 10:45:41 -0300

    cambios

--- formulario-equipo/src/app/app.module.ts
+++ formulario-equipo/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './compartida/header/header.component';
 import { FooterComponent } from './compartida/footer/footer.component';
 import { HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HttpClientModule} from '@angular/common/http';
     RegistroComponent,
     ProductosComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
