7b7147a63a84dce7ab5e60198b95acb5a8e2f567
Ismael Peña Mena <ipenamen@everis.com>
2021-09-29 09:45:04 -0300

    agregando compartida3

--- formulario-equipo/src/app/app.module.ts
+++ formulario-equipo/src/app/app.module.ts
@@ -3,18 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 import { LoginComponent } from './component/login/login.component';
 import { RegistroComponent } from './component/registro/registro.component';
 import { HomeComponent } from './component/home/home.component';
 import { ProductosComponent } from './component/productos/productos.component';
+=======
+import { HeaderComponent } from './compartida/header/header.component';
+import { FooterComponent } from './compartida/footer/footer.component';
+>>>>>>> fa92651 (agregando compartida)
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< HEAD
     LoginComponent,
     RegistroComponent,
     HomeComponent,
     ProductosComponent
+=======
+    HeaderComponent,
+    FooterComponent
+>>>>>>> fa92651 (agregando compartida)
   ],
   imports: [
     BrowserModule,
